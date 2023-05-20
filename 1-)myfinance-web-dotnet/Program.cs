517e6d2569c9a4ad26881a27dbc2098179e4a454
Thalita Leandra de Oliveira Costa <t.de.oliveira.costa@avanade.com>
2023-05-20 00:57:58 -0300

    tela de listagem de plano de contas

--- 1-)myfinance-web-dotnet/Program.cs
+++ 1-)myfinance-web-dotnet/Program.cs
@@ -1,9 +1,15 @@
 using myfinance_web_dotnet_infra;
+using myfinance_web_dotnet_service;
+using myfinance_web_dotnet_service.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MyFinanceDbContext>();
+builder.Services.AddScoped<IPlanoContaService, PlanoContaService>();
+builder.Services.AddScoped<ITransacaoService, TransacaoService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
