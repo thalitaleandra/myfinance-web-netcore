80e31fbac2e9baa97059b1178bc2f2ec3a70ce0c
Thalita Leandra de Oliveira Costa <t.de.oliveira.costa@avanade.com>
2023-05-19 23:04:15 -0300

    fixed it build

--- 4-)myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ 4-)myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -5,10 +5,10 @@ namespace myfinance_web_dotnet_infra;
 
 public class MyFinanceDbContext : DbContext
 {
-    public DbSet<PlanoConta> PlanoConta { get; set; }
-    public DbSet<Transacao> Transacao { get; set; }
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-  {
-    optionsBuilder.useSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;");
-  }
+      public DbSet<PlanoConta> PlanoConta { get; set; }
+      public DbSet<Transacao> Transacao { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=myfinance;Trusted_Connection=True;");
+    }
 }
